3f4e9061e917711d881a9aa28a5be9c64802d5a0
Bahniman <bahnimanborah.borah7@gmail.com>
2020-11-18 21:36:04 +0530

    enhancements

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
 /**
  * 
- * main app
+ * Main app
  */
 
+// defining the UI variables
+
+const form = document.querySelector('#task-form');
+const tasklist = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// creating a method to handle all events
+loadEventListeners();
+
+function loadEventListeners(){
+    
+    // add task event
+    form.addEventListener('submit', addTask);
+
+    function addTask(e){
+
+        if(taskInput.value === ''){
+            alert('Please write a task');
+        }else{
+
+        //create new element to add
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+        //create text node and append
+        const text = document.createTextNode(taskInput.value);
+        li.appendChild(text);
+        //create new link element
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content'; //secondary content means go to right side 
+        // add icon
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+
+        li.appendChild(link);
+
+        //adding to the task list
+
+        tasklist.appendChild(li);
+
+        //clear Input
+        taskInput.value = '';
+
+        }
+
+        e.preventDefault();
+    }
+
+}
